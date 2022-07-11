ba46bf3cab8c93b90f6a3a6264cde519a7dcbfae
Michaela Beakley <bullfrogbst@gmail.com>
2022-07-10 19:05:47 -0500

    begin to rewrite javascript due to messy code. Declare the event listener and game function.

--- index.js
+++ index.js
@@ -1,40 +1,14 @@
 const newGameBtn = document.querySelector('#new-game-btn');
 
+//Check if the button was clicked, and if so, prompt the user to input a number and pass it to the game function.
 newGameBtn.addEventListener('click', () =>{
-    game(parseInt(prompt('How many games do you want to play?')), null, null)
+    let roundCount = parseInt(prompt('How many games do you want to play?'))
+    game(roundCount);
 })
 
-//Start a round as many times as inputted.
-function game(rounds, wins, roundsLeft){
-    for(let i=1; i<rounds; i++){
-        if(roundsLeft == null){
-            roundsLeft = rounds;
-        }
-        playRound(rounds);
+//Make the game function, and call the newRound function for as many times as were passed.
+function game(roundCount){
+    for(let i=0; i<roundCount; i++){
+        newRound();
     }
-}
-
-//playRound function
-function playRound(rounds){
-    let roundsLeft = rounds;
-    let wins = 0;
-
-    let playerInput = prompt("Rock, Paper, or Scissors?");
-    //replace the player input with the lowercase version if it's rock, paper, or scissors.
-    playerInput = playerInput.replace(/Rock|Paper|Scissors/i, playerInput.toLowerCase())
-
-     playerInput = playerInput.replace(',', '')
-    console.log(playerInput)
-
-    //if the input isn't rock, paper, or scissors, then return an error.
-    if(playerInput != 'rock' && playerInput != 'paper' && playerInput != 'scissors'){
-        console.log('Invalid Input');
-    }
-
-    //generate a random input for the computer to choose
-    const rps = ['rock', 'paper', 'scissors']
-    let randomIndex = Math.floor(Math.random() * 4);
-    let compInput = rps[randomIndex];
-
-    console.log(compInput)
 }
