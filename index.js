5b422d771d0f4112297944925ff6dbe0e0c9ed0d
Michaela Beakley <bullfrogbst@gmail.com>
2022-07-13 08:14:29 -0500

    Write out javascript pseudo-code

---
+++ index.js
@@ -0,0 +1,29 @@
+//Get the buttons nodeList and store it in a variable. Also store the gameText into a variable
+
+//Make a gameStats object with various stats for the game
+
+//Add an event listener for the buttons, and check which button was pressed. Call the newInput() function.
+
+//Declare the newInput() function
+
+//If the button that was pressed was the new game button, call the newGame() function
+
+//If the button that was pressed was rock, paper, or scissors, then set gameStats.userInput to the userInput, and then freeze the userInput
+
+//Declare the newGame() function
+
+//Reset all game stats, display "New Game!" for a certain interval of time, and call the newRound function for as many times as the gameStats.roundCount says
+
+//Declare the newRound() function
+
+//Display the initial game stats until either rock, paper, or scissors was chosen
+
+//Once rock, paper or scissors was chosen, display the user's input on the gameText
+
+//After waiting for a certain interval of time, declare the computer's input as a random item from an array
+
+//Check who the winner is, and display it on the game text after a certain interval of time
+
+//Increment the wins or losses value accordingly if it wasn't a tie after a certain amount of time, so the function doesn't get called again for a few seconds
+
+//Unfreeze the userInput value
