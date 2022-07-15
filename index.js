4b5803f3416e526e587cc1c01f8fc75b01ab258d
Michaela Beakley <bullfrogbst@gmail.com>
2022-07-15 16:41:25 -0500

    Select a random computer input and display it on the game text

--- index.js
+++ index.js
@@ -6,9 +6,10 @@ const gameStats = {
     firstTo: 5,
     wins: 0,
     losses: 0,
-    winner: '',
+    gameWinner: '',
     
     userInput: '',
+    computerInput: '',
 
     gameRunning: false,
     roundEmpty: true,
@@ -16,9 +17,12 @@ const gameStats = {
     reset: function(){
         this.wins = 0;
         this.losses = 0;
+
         this.userInput = 'new-game-btn';
         Object.defineProperty(this, "userInput", { configurable: true, writable: true });
 
+        this.computerInput = '';
+        Object.defineProperty(this, "computerInput", { configurable: true, writable: true });
     }
 }
 //Add an event listener for the buttons, and check which button was pressed. Call the newInput() function.
@@ -53,35 +57,40 @@ function newGame(){
         while(gameStats.wins < 5 && gameStats.losses < 5 && gameStats.gameRunning && gameStats.roundEmpty){
                 newRound();
                 gameStats.roundEmpty = false;
-                console.log('New Round!')
+                console.log('New Round!');
         }
     }, 1250);
 }
 
 //Declare the newRound() function
-async function newRound(){
-    console.log(gameStats.userInput)
-    //Check who the winner so far is, and update gameStats.winner to the value
+function newRound(){
+    //Check who the gameWinner so far is, and update gameStats.gameWinner to the value
     if(gameStats.wins > gameStats.losses){
-        gameStats.winner = "You're winning"
+        gameStats.gameWinner = "You're winning";
     } else if(gameStats.wins < gameStats.losses){
-        gameStats.winner = "The computer is winning"
+        gameStats.gameWinner = "The computer is winning";
     } else{
-        gameStats.winner = "Tie"
+        gameStats.gameWinner = "Tie";
     }
     //Display the initial game stats until either rock, paper, or scissors was chosen
-    gameText.innerHTML = `${gameStats.winner}<br><br>You: ${gameStats.wins}<br>Computer: ${gameStats.losses}<br><br>Rock, Paper, or Scissors?`
+    gameText.innerHTML = `${gameStats.gameWinner}<br><br>You: ${gameStats.wins}<br>Computer: ${gameStats.losses}<br><br>Rock, Paper, or Scissors?`
     //Once rock, paper or scissors was chosen, display the user's input on the gameText. Do this by awaiting the newInput() function
     if(gameStats.userInput != 'new-game-btn'){
         console.log(gameStats.userInput)
-        gameText.innerHTML = `${gameStats.winner}<br><br>You: ${gameStats.wins}<br>Computer: ${gameStats.losses}<br><br>Your choice: ${gameStats.userInput}`;
+        gameText.innerHTML = `${gameStats.gameWinner}<br><br>You: ${gameStats.wins}<br>Computer: ${gameStats.losses}<br><br>Your choice: ${gameStats.userInput}`;
+
+        //After waiting for a certain interval of time, declare the computer's input as a random item from an array. Freeze the computerInput property
+        const rps = ['Rock', 'Paper', 'Scissors'];
+        gameStats.computerInput = rps[Math.floor(Math.random() * 3)];
+        Object.defineProperty(gameStats, "computerInput", { configurable: true, writable: false });
+        setTimeout(() =>{
+            gameText.innerHTML += `<br>Computer's choice: ${gameStats.computerInput}`;
+        }, 1250)
+        //Check who the winner is, and display it on the game text after a certain interval of time
+        
+        //Increment the wins or losses value accordingly if it wasn't a tie after a certain amount of time, so the function doesn't get called again for a few seconds
+        
+        //Unfreeze the userInput value
+        
     }
-    //After waiting for a certain interval of time, declare the computer's input as a random item from an array
-    
-    //Check who the winner is, and display it on the game text after a certain interval of time
-    
-    //Increment the wins or losses value accordingly if it wasn't a tie after a certain amount of time, so the function doesn't get called again for a few seconds
-    
-    //Unfreeze the userInput value
-    
 }
