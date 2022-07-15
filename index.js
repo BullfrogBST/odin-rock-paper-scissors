e2ae48ff06a73f58909317e609aaa7bca211fa15
Michaela Beakley <bullfrogbst@gmail.com>
2022-07-15 16:25:57 -0500

    Get user input and successfully display it on the screen

--- index.js
+++ index.js
@@ -16,7 +16,9 @@ const gameStats = {
     reset: function(){
         this.wins = 0;
         this.losses = 0;
-        this.userInput = '';
+        this.userInput = 'new-game-btn';
+        Object.defineProperty(this, "userInput", { configurable: true, writable: true });
+
     }
 }
 //Add an event listener for the buttons, and check which button was pressed. Call the newInput() function.
@@ -29,23 +31,25 @@ buttons.forEach(button =>{
 function newInput(userInput){
 //If the button that was pressed was the new game button, call the newGame() function
     if(userInput == 'new-game-btn'){
+        console.log('ooga booga')
         newGame();
     } else{
-        //If the button that was pressed was rock, paper, or scissors, then set gameStats.userInput to the userInput, and then freeze the userInput
+        //If the button that was pressed was rock, paper, or scissors, then set gameStats.userInput to the userInput, and then freeze the userInput. Call the newRound function
         gameStats.userInput = userInput;
-        Object.defineProperty(gameStats, "userInput", { configurable: false, writable: false });
+        Object.defineProperty(gameStats, "userInput", { configurable: true, writable: false });
+        newRound();
     }
 }
+
 //Declare the newGame() function
 function newGame(){
 //Reset all game stats, display "New Game!" for a certain interval of time, and call the newRound function for as many times as the gameStats.firstTo says.
     gameStats.reset();
     gameText.textContent = "New Game!";
     gameStats.gameRunning = true;
+    gameStats.roundEmpty = true;
 
     setTimeout(() =>{
-        console.log(gameStats.wins)
-        console.log(gameStats.losses)
         while(gameStats.wins < 5 && gameStats.losses < 5 && gameStats.gameRunning && gameStats.roundEmpty){
                 newRound();
                 gameStats.roundEmpty = false;
@@ -55,19 +59,29 @@ function newGame(){
 }
 
 //Declare the newRound() function
-function newRound(){
-    console.log('debug')
+async function newRound(){
+    console.log(gameStats.userInput)
+    //Check who the winner so far is, and update gameStats.winner to the value
+    if(gameStats.wins > gameStats.losses){
+        gameStats.winner = "You're winning"
+    } else if(gameStats.wins < gameStats.losses){
+        gameStats.winner = "The computer is winning"
+    } else{
+        gameStats.winner = "Tie"
+    }
+    //Display the initial game stats until either rock, paper, or scissors was chosen
+    gameText.innerHTML = `${gameStats.winner}<br><br>You: ${gameStats.wins}<br>Computer: ${gameStats.losses}<br><br>Rock, Paper, or Scissors?`
+    //Once rock, paper or scissors was chosen, display the user's input on the gameText. Do this by awaiting the newInput() function
+    if(gameStats.userInput != 'new-game-btn'){
+        console.log(gameStats.userInput)
+        gameText.innerHTML = `${gameStats.winner}<br><br>You: ${gameStats.wins}<br>Computer: ${gameStats.losses}<br><br>Your choice: ${gameStats.userInput}`;
+    }
+    //After waiting for a certain interval of time, declare the computer's input as a random item from an array
+    
+    //Check who the winner is, and display it on the game text after a certain interval of time
+    
+    //Increment the wins or losses value accordingly if it wasn't a tie after a certain amount of time, so the function doesn't get called again for a few seconds
+    
+    //Unfreeze the userInput value
+    
 }
-//Display the initial game stats until either rock, paper, or scissors was chosen
-
-//Once rock, paper or scissors was chosen, display the user's input on the gameText
-
-//After waiting for a certain interval of time, declare the computer's input as a random item from an array
-
-//Check who the winner is, and display it on the game text after a certain interval of time
-
-//Increment the wins or losses value accordingly if it wasn't a tie after a certain amount of time, so the function doesn't get called again for a few seconds
-
-//Check who the winner so far is, and update gameStats.winner to the value
-
-//Unfreeze the userInput value
